cfca4fd662a5cf12ac659dca8ac2cf8e7bae519d
zuhedshaikh95 <zuhed95@gmail.com>
2023-04-16 23:27:03 +0530

    All done

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -62,7 +62,7 @@ const authLogin = async (request, response) => {
             const serialised =  {
                 httpOnly: true,
                 secure: NODE_ENV === 'production',
-                sameSite: 'lax',
+                sameSite: NODE_ENV === 'production' ? 'none' : 'strict',
                 path: '/',
               };
 
