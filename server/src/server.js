eca9701f400a3627450f6d88102b13b27055c893
zuhedshaikh95 <zuhed95@gmail.com>
2023-04-23 17:06:01 +0530

    fix: country from IP

--- server/src/server.js
+++ server/src/server.js
@@ -37,7 +37,7 @@ app.get('/', (request, response) => {
 });
 
 app.get('/ip', (request, response) => {
-    const ip = request.socket.remoteAddress || '';
+    const ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress || '';
 
     return response.send({ ip });
 })
