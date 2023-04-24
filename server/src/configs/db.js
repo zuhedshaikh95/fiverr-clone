995e7bf67408422de43c7827a73b46048f4fe985
zuhedshaikh95 <zuhed95@gmail.com>
2023-04-24 14:11:50 +0530

    update: .env variable

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
 const connect = () => {
-    return mongoose.connect(process.env.MONGODB_URL);
+    return mongoose.connect(process.env.MONGODB_URI);
 };
 
 module.exports = connect;
