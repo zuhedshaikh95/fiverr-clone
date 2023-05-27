38c2b4ff2d59f42976b7ad920bc384f36c4a4f67
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-28 02:05:28 +0530

    add: persist user from localStorage

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,40 +1,57 @@
 import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import Slider from 'react-slick';
-import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
-import { axiosFetch, getCurrentUser } from "../../utils";
-import "./Navbar.scss";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { axiosFetch } from "../../utils";
+import { useQuery } from "@tanstack/react-query";
+import { Loader } from "../../components";
 
+import "./Navbar.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useRecoilState } from "recoil";
+import { userAtom } from "../../atoms";
 
 const Navbar = () => {
-  const currentUser = getCurrentUser();
   const [showMenu, setShowMenu] = useState(false);
   const [showPanel, setShowPanel] = useState(false);
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const [user, setUser] = useRecoilState(userAtom);
 
   const isActive = () => {
     window.scrollY > 0 ? setShowMenu(true) : setShowMenu(false);
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', isActive);
+    window.addEventListener("scroll", isActive);
     return () => {
-      window.removeEventListener('scroll', isActive);
-    }
+      window.removeEventListener("scroll", isActive);
+    };
   }, []);
 
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["me"],
+    queryFn: () =>
+      axiosFetch.get(`/auth/me`)
+        .then(({ data }) => {
+          setUser(data.user);
+          return data.user;
+        })
+        .catch(({ response }) => {
+          console.log(response.data.message);
+          return null;
+        }),
+  });
 
   const menuLinks = [
-    { path: '/gigs?category=design', name: 'Graphics & Design' },
-    { path: '/gigs?category=video', name: 'Video & Animation' },
-    { path: '/gigs?category=books', name: 'Writing & Translation' },
-    { path: '/gigs?category=ai', name: 'AI Services' },
-    { path: '/gigs?category=social', name: 'Digital Marketing' },
-    { path: '/gigs?category=voice', name: 'Music & Audio' },
-    { path: '/gigs?category=wordpress', name: 'Programming & Tech' },
+    { path: "/gigs?category=design", name: "Graphics & Design" },
+    { path: "/gigs?category=video", name: "Video & Animation" },
+    { path: "/gigs?category=books", name: "Writing & Translation" },
+    { path: "/gigs?category=ai", name: "AI Services" },
+    { path: "/gigs?category=social", name: "Digital Marketing" },
+    { path: "/gigs?category=voice", name: "Music & Audio" },
+    { path: "/gigs?category=wordpress", name: "Programming & Tech" },
   ];
 
   const settings = {
@@ -49,39 +66,38 @@ const Navbar = () => {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-        }
+        },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 3,
-        }
+        },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 2,
-        }
-      }
-    ]
-  }
+        },
+      },
+    ],
+  };
 
   const handleLogout = async () => {
     try {
-      await axiosFetch.post('/auth/logout');
-      localStorage.removeItem('currentUser');
-      navigate('/');
-    }
-    catch ({ response }) {
+      await axiosFetch.post("/auth/logout");
+      setUser(null);
+      navigate("/");
+    } catch ({ response }) {
       console.log(response.data);
     }
-  }
+  };
 
   return (
-    <nav className={showMenu || pathname !== '/' ? 'navbar active' : 'navbar'}>
+    <nav className={showMenu || pathname !== "/" ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
-          <Link to='/' className="link">
+          <Link to="/" className="link">
             <span className="text">fiverr</span>
           </Link>
           <span className="dot">.</span>
@@ -92,45 +108,73 @@ const Navbar = () => {
             <span>Fiverr Business</span>
             <span>Explore</span>
             <span>English</span>
-            {!currentUser.isSeller && <span>Become a Seller</span>}
+            {!user?.isSeller && <span>Become a Seller</span>}
           </div>
-          {!currentUser && <span><Link to='/login' className="link">Sign in</Link></span>}
-          {!currentUser.username && <button className={showMenu || pathname !== '/' ? 'join-active' : ''}><Link to='/register' className="link">Join</Link></button>}
-          {currentUser.username && (
-            <div className="user" onClick={() => setShowPanel(!showPanel)}>
-              <img src={currentUser.image || '/media/noavatar.png'} />
-              <span>{currentUser?.username}</span>
-              {showPanel && (
-                <div className="options">
-                  {
-                    currentUser?.isSeller && (
+          {!user && (
+            <span>
+              <Link to="/login" className="link">
+                Sign in
+              </Link>
+            </span>
+          )}
+          {!user && (
+            <button
+              className={showMenu || pathname !== "/" ? "join-active" : ""}
+            >
+              <Link to="/register" className="link">
+                Join
+              </Link>
+            </button>
+          )}
+          {isLoading ? (
+            <Loader />
+          ) : (
+            user && (
+              <div className="user" onClick={() => setShowPanel(!showPanel)}>
+                <img src={user.image} />
+                <span>{user?.username}</span>
+                {showPanel && (
+                  <div className="options">
+                    {user?.isSeller && (
                       <>
-                        <Link className="link" to='/my-gigs'>Gigs</Link>
-                        <Link className="link" to='/organize'>Add New Gig</Link>
+                        <Link className="link" to="/my-gigs">
+                          Gigs
+                        </Link>
+                        <Link className="link" to="/organize">
+                          Add New Gig
+                        </Link>
                       </>
-                    )
-                  }
-                  <Link className="link" to='/orders'>Orders</Link>
-                  <Link className="link" to='/messages'>Messages</Link>
-                  <Link className="link" to='/' onClick={handleLogout}>Logout</Link>
-                </div>
-              )}
-            </div>
+                    )}
+                    <Link className="link" to="/orders">
+                      Orders
+                    </Link>
+                    <Link className="link" to="/messages">
+                      Messages
+                    </Link>
+                    <Link className="link" to="/" onClick={handleLogout}>
+                      Logout
+                    </Link>
+                  </div>
+                )}
+              </div>
+            )
           )}
         </div>
       </div>
-      {(showMenu || pathname !== '/') && <>
-        <hr />
-        <Slider className="menu" {...settings}>
-          {
-            menuLinks.map(({ path, name }) => (
+      {(showMenu || pathname !== "/") && (
+        <>
+          <hr />
+          <Slider className="menu" {...settings}>
+            {menuLinks.map(({ path, name }) => (
               <div key={name} className="menu-item">
-                <Link className='link' to={path}>{name}</Link>
+                <Link className="link" to={path}>
+                  {name}
+                </Link>
               </div>
-            ))
-          }
-        </Slider>
-      </>}
+            ))}
+          </Slider>
+        </>
+      )}
     </nav>
   );
 };
