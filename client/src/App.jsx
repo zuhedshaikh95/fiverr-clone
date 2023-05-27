ea8eb6a79220d7569012a4c47d211b566847f78d
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-28 02:08:26 +0530

    add: RecoilRoot

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,91 @@
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from 'react-hot-toast';
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
-import { Navbar, PrivateRoute } from './components';
-import { Home, Footer, Gig, Gigs, MyGigs, Add, Orders, Message, Messages, Login, Register, Pay, Success, NotFound } from './pages';
-import './App.scss';
+import { Navbar, PrivateRoute } from "./components";
+import {
+  Home,
+  Footer,
+  Gig,
+  Gigs,
+  MyGigs,
+  Add,
+  Orders,
+  Message,
+  Messages,
+  Login,
+  Register,
+  Pay,
+  Success,
+  NotFound,
+} from "./pages";
+import "./App.scss";
+import { RecoilRoot } from "recoil";
 
 const paths = [
-  { path: '/', element: <Home /> },
-  { path: '/gig/:_id', element: <Gig /> },
-  { path: '/gigs', element: <Gigs /> },
-  { path: '/login', element: <Login /> },
-  { path: '/register', element: <Register /> },
-  { path: '/orders', element: <PrivateRoute><Orders /></PrivateRoute> },
-  { path: '/organize', element: <PrivateRoute><Add /></PrivateRoute> },
-  { path: '/my-gigs', element: <PrivateRoute><MyGigs /></PrivateRoute> },
-  { path: '/message/:conversationID', element: <PrivateRoute><Message /></PrivateRoute> },
-  { path: '/messages', element: <PrivateRoute><Messages /></PrivateRoute> },
-  { path: '/pay/:_id', element: <PrivateRoute><Pay /></PrivateRoute> },
-  { path: '/success', element: <PrivateRoute><Success /></PrivateRoute> },
-  { path: '*', element: <NotFound /> }
+  { path: "/", element: <Home /> },
+  { path: "/gig/:_id", element: <Gig /> },
+  { path: "/gigs", element: <Gigs /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  {
+    path: "/orders",
+    element: (
+      <PrivateRoute>
+        <Orders />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/organize",
+    element: (
+      <PrivateRoute>
+        <Add />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/my-gigs",
+    element: (
+      <PrivateRoute>
+        <MyGigs />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/message/:conversationID",
+    element: (
+      <PrivateRoute>
+        <Message />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/messages",
+    element: (
+      <PrivateRoute>
+        <Messages />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/pay/:_id",
+    element: (
+      <PrivateRoute>
+        <Pay />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "/success",
+    element: (
+      <PrivateRoute>
+        <Success />
+      </PrivateRoute>
+    ),
+  },
+  { path: "*", element: <NotFound /> },
 ];
-
 
 function App() {
   const queryClient = new QueryClient();
@@ -32,22 +96,24 @@ function App() {
         <Outlet />
         <Footer />
       </QueryClientProvider>
-    )
-  }
+    );
+  };
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <Layout />,
-      children: paths.map(({ path, element }) => ({ path, element }))
-    }
-  ])
+      children: paths.map(({ path, element }) => ({ path, element })),
+    },
+  ]);
 
   return (
     <div className="App">
-      <RouterProvider router={router} />
-      <Toaster />
+      <RecoilRoot>
+        <RouterProvider router={router} />
+        <Toaster />
+      </RecoilRoot>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
