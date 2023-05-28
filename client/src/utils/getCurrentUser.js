6da6cbfec5b616c660ccd5efdec6a5a230cfc595
zuhedshaikh95 <zuhed95@gmail.com>
2023-05-28 15:35:31 +0530

    fix: removed localStorage module

--- client/src/utils/getCurrentUser.js
+++
@@ -1,5 +0,0 @@
-const getCurrentUser = () => {
-    return JSON.parse(localStorage.getItem('currentUser')) || false;
-}
-
-export default getCurrentUser;
--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,4 @@
 export { default as axiosFetch } from './axiosFetch';
-export { default as getCurrentUser } from './getCurrentUser';
 export { default as generateImageURL } from './generateImageURL';
 export { default as userLogout } from './userLogout';
 export { default as countriesFlags } from './countriesFlags';
